f1975f50a7ed7543b1558e2423fdd5818aa73ca6
DebDoDab <debbius3@gmail.com>
2020-03-21 20:04:34 +0300

    First commit

---
+++ studypunch/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for studypunch project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studypunch.settings')
+
+application = get_wsgi_application()
