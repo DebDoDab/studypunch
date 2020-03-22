47d0cf12da44b1374ce324d23cd0f5f86546f999
DebDoDab <debbius3@gmail.com>
2020-03-22 19:05:30 +0300

    fix(settings) Api v0.1 ready

--- studypunch/settings.py
+++ studypunch/settings.py
@@ -96,8 +96,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 20,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',),
 }
 
 
