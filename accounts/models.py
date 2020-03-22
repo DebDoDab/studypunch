3bec801fe777590d17ed53a702e492e3056c24f1
DebDoDab <debbius3@gmail.com>
2020-03-22 13:16:51 +0300

    refactor(accounts) Renamed MyUserManager to CustomUserManager

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from groups.models import Group
 from django.contrib.auth.management.commands.createsuperuser import Command
 
 
-class MyUserManager(UserManager):
+class CustomUserManager(UserManager):
     def _create_user(self, username, email, password, group_id=None, **extra_fields):
         """
         Create and save a user with the given username, email, and password.
@@ -13,28 +13,12 @@ class MyUserManager(UserManager):
         if not username:
             raise ValueError('The given username must be set')
         email = self.normalize_email(email)
-        group_id = extra_fields.pop('group')
         username = self.model.normalize_username(username)
+        group_id = extra_fields.pop('group')
         user = self.model(username=username, email=email, group=Group.objects.get(id=group_id), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, email, password, **extra_fields)
-
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -42,5 +26,5 @@ class User(AbstractUser):
     doneHomework = models.ManyToManyField(Homework)
 
     REQUIRED_FIELDS = ['group', 'email']
-    objects = MyUserManager()
+    objects = CustomUserManager()
 
