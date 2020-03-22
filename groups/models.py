ab9276b18458dd9c7db39e9896ab23b398e6ef9c
DebDoDab <debbius3@gmail.com>
2020-03-22 03:34:41 +0300

    feature(groups) Added tokens

--- groups/models.py
+++ groups/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.utils.crypto import get_random_string
 
 
 class Group(models.Model):
     name = models.CharField(max_length=32)
+    token = models.CharField(max_length=32)
 
     @staticmethod
     def createAdminGroup():
+        token = Group.generateToken()
         try:
-            group = Group.objects.get(name='admin')
+            group = Group.objects.get(name='admin', token=token)
         except Group.DoesNotExist:
-            group = Group.objects.create(name='admin')
+            group = Group.objects.create(name='admin', token=token)
         return group.id
+
+    @staticmethod
+    def generateToken():
+        token = get_random_string(32)
+        while Group.objects.filter(token=token).exists():
+            token = get_random_string(32)
+        return token
