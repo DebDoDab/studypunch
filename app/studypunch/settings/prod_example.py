12c06d77585b6cd965cb785e8d7b77838e2e7591
DebDoDab <debbius3@gmail.com>
2020-07-20 19:01:34 +0300

    Logging configuration Edited settings/prod_example.py Now there is a logging section that will log messages in docker

--- app/studypunch/settings/prod_example.py
+++ app/studypunch/settings/prod_example.py
@@ -10,3 +10,29 @@ SECRET_KEY = 'yadayadayada'
 # Rewrite ALLOWED_HOSTS to access django from your production server
 ALLOWED_HOSTS = ['vadi.tel', 'api.vadi.tel', 'localhost', '0.0.0.0']
 
+# Logging to a console
+# To see logs run `docker-compose -f prod.yml logs python`
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '[DJANGO] %(levelname)s %(asctime)s %(module)s '
+                      '%(name)s.%(funcName)s:%(lineno)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
