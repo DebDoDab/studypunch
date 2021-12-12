09286b828c2873dae240127aff1431633c3261d6
DebDoDab <debbius3@gmail.com>
2021-12-13 02:44:38 +0300

    add colors to subjects

--- app/subjects/models.py
+++ app/subjects/models.py
@@ -6,6 +6,11 @@ class Subject(models.Model):
     """Model for subjects (e.g. math)"""
     name = models.CharField(max_length=32, verbose_name="Name of the subject")
     group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name="Link to the adjacent group")
+    color = models.CharField(
+        max_length=7,
+        default="#123456",
+        verbose_name="Color of the subject in hex (e.g. '#123456')",
+    )
 
     def __str__(self):
         return self.name
--- app/subjects/serializers.py
+++ app/subjects/serializers.py
@@ -6,12 +6,14 @@ from .models import Subject
 class SubjectSerializer(serializers.ModelSerializer):
     """Serializer for Subject model"""
     group_id = serializers.IntegerField(write_only=True)
+    color = serializers.CharField(max_length=7, default="#123456")
 
     def create(self, validated_data):
+        validated_data['color'] = f'#123456'
         validated_data['group'] = Group.objects.get(id=validated_data.get('group_id'))
         validated_data.pop('group_id')
         return Subject.objects.create(**validated_data)
 
     class Meta:
         model = Subject
-        fields = ['id', 'name', 'url', 'group_id']
+        fields = ['id', 'name', 'color', 'url', 'group_id']
